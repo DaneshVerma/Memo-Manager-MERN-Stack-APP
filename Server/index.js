562edc8fb8b374cce6d9b74fa435daa129dfaf69
DaneshwarVerma <techydanesh@gmail.com>
2024-07-07 13:44:45 +0530

    added Dotenv and colud database string to server side

--- Server/index.js
+++ Server/index.js
@@ -3,13 +3,15 @@ import mongoose from "mongoose"; // for Mongodb database accesseblity
 import bodyParser from "body-parser"; //middleware
 import { deletefn, retrivefn, addnewfn, updatefn } from "./routes/routes.js"; // functional beahaviours of routes
 import cors from "cors"; // middelware
+import dotenv from "dotenv";
 const app = express(); // creating express app
-const port = 3000;  
+dotenv.config()
+const port = process.env.PORT || 3000;  
 
 app.use(cors()); // middelware to accept resource request from diffrent domain. 
 app.use(bodyParser.urlencoded({ extended: true })); // to retrive data from api requests
 app.use(express.json()); // data formatting to json
-await mongoose.connect("mongodb://localhost:27017/task"); // connection to database
+await mongoose.connect(process.env.DB_URl); // connection to database
 
 // declaring different routes and functions 
 app.post("/new", addnewfn);
@@ -18,5 +20,5 @@ app.put("/update", updatefn);
 app.delete("/delete", deletefn);
 
 app.listen(port, () => {
-  console.log("server listing on : http://localhost:3000/");
+  console.log(`server listing on : http://localhost:${port}/`);
 });
