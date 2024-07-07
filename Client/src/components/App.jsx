2c49c8833693c166b5bffaedf7822aac876b3c33
DaneshwarVerma <techydanesh@gmail.com>
2024-07-07 22:03:30 +0530

    establilished connection with backend server and added connection string as a env variable

--- Client/src/components/App.jsx
+++ Client/src/components/App.jsx
@@ -6,9 +6,8 @@ import Note from "./Note";
 import CreateArea from "./CreateArea"; // above rest are react components
 import axios from "axios"; // for conneting with the server to communicate
 import { v4 as uuidv4 } from "uuid"; // for assining each note element a unique id
-import dotenv from "dotenv"
 
-dotenv.config()
+const backendURL = import.meta.env.VITE_BACKEND_SERVER;
 
 function App() {
   const [notes, setNotes] = useState([]); // to store notes data retrived from server
@@ -16,7 +15,7 @@ function App() {
 
   useEffect(() => {
     // fetches the existing saved note data from MongoDB server
-    axios.get(`${process.env.BACKEND_SERVER}/data`).then((response) => {
+    axios.get(`${backendURL}/data`).then((response) => {
       setNotes(response.data);
     });
   }, []);
@@ -24,7 +23,7 @@ function App() {
   function addNote(newNote) {
     // this function sends the new note entry to server and fecthes the the data containing new entry
     axios
-      .post(`${process.env.BACKEND_SERVER}/new`, {
+      .post(`${backendURL}/new`, {
         title: newNote.title,
         note: newNote.content,
         noteid: uuidv4(),
@@ -39,7 +38,7 @@ function App() {
   function deleteNote(noteid) {
     // this function sends delete request for seleted note by its id facthes the new data after deletation
     axios
-      .delete(`${process.env.BACKEND_SERVER}/delete`, { data: { id: noteid } })
+      .delete(`${backendURL}/delete`, { data: { id: noteid } })
       .then((response) => {
         setNotes(response.data);
       });
@@ -51,37 +50,39 @@ function App() {
   function updateNote(note) {
     // after editing this function send's an update request to the server with the data to be updated and fecthes new data with updated changes.
     console.log(note);
-    axios.put(`${process.env.BACKEND_SERVER}/update`, note).then((response) => {
+    axios.put(`${backendURL}/update`, note).then((response) => {
       setNotes(response.data);
     });
-  } 
+  }
 
-  // this is just a constent variable containing information about me a fellow developer not included it into the other notes in the database so that only this note object can be the immutable one. 
+  // this is just a constent variable containing information about me a fellow developer not included it into the other notes in the database so that only this note object can be the immutable one.
   const myAbout = {
     index: "me",
     title: "About",
     note: "I'm Danesh one who made this MERN stack based project. the application's Concept is inspired from Google Keep. click at the header of this site to see the source code & click On my name below to contact me.",
   };
   return (
-// rendering all the required components along with sending required props to each.
+    // rendering all the required components along with sending required props to each.
     <div>
       <Header />
       <CreateArea onAdd={addNote} editable={noteToEdit} onUpdate={updateNote} />
       <div className="container">
         <AboutNote title={myAbout.title} content={myAbout.note} />
-        {notes.map((noteItem, index) => {
-          return (
-            <Note
-              key={index}
-              id={index}
-              title={noteItem.title}
-              content={noteItem.note}
-              noteId={noteItem.id}
-              onDelete={deleteNote}
-              onEdit={editNote}
-            />
-          );
-        })}
+        {Array.isArray(notes)
+          ? notes.map((noteItem, index) => {
+              return (
+                <Note
+                  key={index}
+                  id={index}
+                  title={noteItem.title}
+                  content={noteItem.note}
+                  noteId={noteItem.id}
+                  onDelete={deleteNote}
+                  onEdit={editNote}
+                />
+              );
+            })
+          : null}
       </div>
       <Footer />
     </div>
