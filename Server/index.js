f7b7088a4a0e11a9f5b3b0a92aca3e168718a38c
DaneshwarVerma <techydanesh@gmail.com>
2024-07-09 11:33:31 +0530

    added error handling in Databse connection

--- Server/index.js
+++ Server/index.js
@@ -9,18 +9,25 @@ dotenv.config();
 const port = 3000;
 
 const corsOptions = {
-  origin: 'https://memos-manager.onrender.com/'|| 'http://localhost:5173', // Allow requests from your frontend's origin
-  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+  origin: "https://memos-manager.onrender.com/" || "http://localhost:5173", // Allow requests from your frontend's origin
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
 };
 
 app.use(cors(corsOptions)); // middelware to accept resource request from diffrent domain.
 app.use(bodyParser.urlencoded({ extended: true })); // to retrive data from api requests
 app.use(express.json()); // data formatting to json
-await mongoose.connect(process.env.DATABASE_URL); // connection to database
+
+try {
+  await mongoose.connect(process.env.DATABASE_URL); // connection to database
+  console.log("connected to data Base")
+} catch (error) {
+  console.error(error.message); // Prints the error message
+  console.error(error.stack);
+}
 
 // declaring different routes and functions
 app.get("/", (req, res) => {
-  res.send('<h1>Hi i am live</h1>').status(200);
+  res.send("<h1>Hi i am live</h1>").status(200);
 });
 app.post("/new", addnewfn);
 app.get("/data", retrivefn);
