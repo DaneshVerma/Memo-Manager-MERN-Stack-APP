1ab0f381701c85951c1faca0f4818c1c8a0f12a7
Danesh <161022737+DaneshVerma@users.noreply.github.com>
2024-09-05 11:10:51 +0530

    Update Footer.jsx

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
   return (
     <footer>
-      <div>Made BY <a href="https://www.instagram.com/thedanesh7/">@Danesh</a></div>
+      <div>Made BY <a target="_blank" href="https://www.instagram.com/thedanesh7/">@Danesh</a></div>
     </footer>
   );
 }
